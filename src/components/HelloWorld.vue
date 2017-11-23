6c716fbd140788d85c52ee67dd10bca14815f53e
wanchenglong <iwancl_217@163.com>
2017-11-23 23:44:01 +0800

    全局修改文件名称及name，title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,23 +27,27 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  created() {
+    console.log(123)
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
 h1, h2 {
   font-weight: normal;
 }
 ul {
   list-style-type: none;
   padding: 0;
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
 }

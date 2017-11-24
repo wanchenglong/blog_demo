950f75e5f8d8c854e0a465227bb7ab87c7c88933
wanchenglong <iwancl_217@163.com>
2017-11-24 22:49:25 +0800

    整屏

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,23 @@
 <template>
     <router-view/>
 </template>
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+  	html, body {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+		width: 100%;
+		overflow: hidden;
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		color: #fff;
+	}	
+</style>
+
+
 
